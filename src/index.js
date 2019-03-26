6d226827cf6ffd0fdb3d5c58fa7fe6f87c5d440b
Erik Ziegler <erik.ziegler@student.hpi.uni-potsdam.de>
2019-03-27 00:51:34 +0100

    Export PayPalSDKWrapper in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import SmartPaymentButtons from './components/SmartPaymentButtons';
+import PayPalSDKWrapper from './components/PayPalSDKWrapper';
+
+export { PayPalSDKWrapper };
 
 export default SmartPaymentButtons;
