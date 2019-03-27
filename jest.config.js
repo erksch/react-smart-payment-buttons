25eb1d34f6cc025cc15f88f752eaddabcd2af92b
Erik Ziegler <erik.ziegler@student.hpi.uni-potsdam.de>
2019-03-27 16:03:38 +0100

    Exclude index.js from coverage

--- jest.config.js
+++ jest.config.js
@@ -27,9 +27,10 @@ module.exports = {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    "/src/index.js",
+    "/node_modules/"
+  ],
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
